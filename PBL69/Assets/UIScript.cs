d39c60a19e9a7027e465a8a51149a459a122f3d1
Erlandfatur <151422818+Erlandfatur@users.noreply.github.com>
2024-06-12 08:47:30 +0700

    update

---
+++ PBL69/Assets/UIScript.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    public void GotoMain(){
+        SceneManager.LoadScene(1);
+    }
+    
+}
